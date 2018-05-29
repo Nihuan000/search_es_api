8bb25c26aa0b3ed77f6a04f5b40897e89095e4ba
Nihuan <nihuan@isoubu.com>
2018-05-29 17:27:58 +0800

    代码初始化

---
+++ app/utils/hooks.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+ * Created by PyCharm.
+ * User: nihuan
+ * Date: 18-5-29
+ * Time: 下午5:03
+ * Desc: 
+ """
+__author__ = 'nihuan'
+
+
+# 索引字典配置
+def settings(req, res, resource):
+    return
+
+
+# 索引映射
+def mappings(req, res, resource):
+    return
